9887ff23655fc233ce350ad5a17f9d57585ee493
Продащук Вадим <vadosprodashik@gmail.com>
2023-09-29 20:13:26 +0300

    Привет

--- p.pas
+++ p.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  Print('Привет мир!')
+var a := ReadInteger;
+case a of
+  4..10:Print('Доброе утро, мир!');
+  11..16:Print('Добрый вечер, мир!');
+  17..22:Print('Доброй ночи, мир!');
+  else Print('Доброй ночи, мир!');
+end;
 end.
